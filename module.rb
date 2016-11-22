73bda5a138fa9e23cb7a8d2ef54a07cd3d23b6b2
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-11-22 12:13:03 -0800

    Implements horizontal and vertical moves for SlidingPiece

--- module.rb
+++ module.rb
@@ -1,12 +1,15 @@
+require 'byebug'
+
 module SlidingPiece
-DIAG_DELTAS = [ [1,1],
+  DIAG_DELTAS = [ [1,1],
                 [-1,1],
                 [-1,-1],
                 [1,-1]]
-R_C_DELTAS = [  [1,0],
+  R_C_DELTAS = [  [1,0],
                 [0,1],
                 [-1,0],
                 [0,-1]]
+
   def moves
     possible_moves = []
     permisions = self.move_dirs
@@ -27,5 +30,17 @@ R_C_DELTAS = [  [1,0],
     all_diags
   end
 
+  def row_col_moves
+    all_row_col_moves = []
+    R_C_DELTAS.each do |delta|
+      7.times do |spacer|
+        byebug
+        new_x = position[0] + (delta[0] * (spacer + 1))
+        new_y = position[1] + (delta[1] * (spacer + 1))
+        all_row_col_moves << [new_x, new_y]
+      end
+    end
+    all_row_col_moves
+  end
 
 end
