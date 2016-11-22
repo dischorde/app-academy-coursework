ecac4d9c891c99a55569e55110b103e8689ffaad
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-11-22 10:39:26 -0800

    Initializes NullPiece class

--- piece.rb
+++ piece.rb
@@ -1,8 +1,20 @@
 class Piece
-  def initialize(name, color, position, board)
-    @name = name
+  def initialize(color, symbol, position, board)
     @color = color
+    @symbol = symbol
     @position = position
     @board = board
+  end
+end
+
+class NullPiece < Piece
+  include Singleton
+  attr_reader :color, :symbol
+
+  def initialize
+    @color = nil
+    @symbol = nil
+    @position = nil
+    @board = nil
   end
 end
