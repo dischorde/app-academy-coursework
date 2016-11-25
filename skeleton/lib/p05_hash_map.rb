f5bc631cac4eca965549855df2ddc6dd6a72345a
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-11-25 15:19:45 -0800

    Create linked list and hash map

--- skeleton/lib/p05_hash_map.rb
+++ skeleton/lib/p05_hash_map.rb
@@ -2,6 +2,9 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+
+  include Enumerable
+
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,27 +13,44 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if @count == num_buckets
+
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
+
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1 if bucket(key).remove(key)
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |link|
+        prc.call(link.key, link.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,9 +62,21 @@ class HashMap
   end
 
   def resize!
+    new_bucket_size = num_buckets * 2
+    new_store = Array.new(new_bucket_size) { LinkedList.new }
+
+    @store.each do |bucket|
+      bucket.each do |link|
+        bucket_index = link.key.hash % new_bucket_size
+        new_store[bucket_index].append(link.key, link.val)
+      end
+    end
+
+    @store = new_store
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
