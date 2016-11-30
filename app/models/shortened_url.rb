f13d70b3e066a1451427ebccfacfe489cae20384
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-11-30 14:09:07 -0800

    Add associations between users and shortened_urls

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,29 @@
+
+
 class ShortenedUrl < ActiveRecord::Base
-  validates :short_url, :user_id, presence: true, uniqueness: true
+  validates  :user_id, presence: true, uniqueness: true
+  validates  :short_url, uniqueness: true
+
+  belongs_to :submitter,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :user_id
+
+  def self.random_code
+    new_url = SecureRandom.urlsafe_base64
+    while ShortenedUrl.exists?(short_url: new_url)
+      new_url = SecureRandom.urlsafe_base64
+    end
+    new_url
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    shortened_url = ShortenedUrl.random_code
+    ShortenedUrl.create!(user_id: user.id, long_url: long_url,
+                         short_url: shortened_url)
+
+  end
+
+
 
 end
