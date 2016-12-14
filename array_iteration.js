f29237bc4ec309882dc3c86abdd630a5a09b5207
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-12-14 15:00:48 -0800

    adds array_iteration.js

--- array_iteration.js
+++ array_iteration.js
@@ -13,3 +13,17 @@ Array.prototype.bubbleSort = function(cb) {
   }
   return this;
 };
+
+
+String.prototype.subString = function(){
+  let subs = [];
+  for (let i = 0; i <= this.length - 1; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      subs.push(this.slice(i, j));
+    }
+  }
+  return subs;
+};
+
+
+console.log("cat".subString());
