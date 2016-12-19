5fca80a5cddab0e344d98ca6bfe43886ba884e05
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-12-19 12:12:29 -0800

    Complete TicTacToe

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ const View = require('./ttt-view.js');
 const Game = require('./game.js');
 
 $( () => {
-  // Your code here
+  const game = new Game();
+  const view = new View(game, $('.ttt'));
+  view.bindEvents();
 });
