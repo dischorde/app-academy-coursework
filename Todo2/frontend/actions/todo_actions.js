fdb93ad1e977765b0c2be23faee35b14629c7f5b
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-03 13:51:52 -0800

    Fetch todos

---
+++ Todo2/frontend/actions/todo_actions.js
@@ -0,0 +1,24 @@
+import * as TodoAPIUtil from '../util/todo_api_util';
+
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
+export const RECEIVE_TODO = "RECEIVE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+
+export const receiveTodo = todo => ({
+  type: RECEIVE_TODO,
+  todo
+});
+
+export const receiveTodos = todos => ({
+  type: RECEIVE_TODOS,
+  todos
+});
+
+export const removeTodo = id => ({
+  type: REMOVE_TODO,
+  id
+});
+
+export const fetchTodos = () => dispatch => (
+  TodoAPIUtil.fetchTodos().then(todos => dispatch(receiveTodos(todos)))
+);
---
+++ Todo2/frontend/components/todo_list/todo_list.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import TodoListItem from './todo_list_item.jsx';
+import TodoForm from './todo_form.jsx';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+          {
+            this.props.todos.map((todo, i) =>
+            <TodoListItem
+              key={i}
+              removeTodo={this.props.removeTodo}
+              receiveTodo={this.props.receiveTodo}
+              todo={todo}
+            />)
+          }
+        </ul>
+        <TodoForm receiveTodo={this.props.receiveTodo} />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
