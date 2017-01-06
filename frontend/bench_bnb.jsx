28b7149f7adaa1ce07371126cc2796e4522ba16f
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-06 14:32:16 -0800

    Setup Auth Backend and SessionApiUtil

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,8 +1,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {login, logout, signup} from './util/session_api_util.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    // window.signup = signup;
+    // window.login = login;
+    // window.logout = logout;
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
